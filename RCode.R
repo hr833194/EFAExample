44d97a6eb8147efe20207d382fa8de531ba980a9
Hera Arif <heraarif94@live.com>
2024-04-02 01:57:05 -0300

    adding data

---
+++ RCode.R
@@ -0,0 +1,61 @@
+library(psych)
+library(readxl)
+library(REdaS)
+library(paran)
+library(EFA.dimensions)
+library(ltm)
+options(max.print = 99999)
+
+#Attach the dataset
+efadataset <- read_excel("C:/Users/heraa/Documents/University/Doctoral Directed Studies/Guest Lecture/surveyData.xlsx")
+View(efadataset)        
+attach(efadataset)
+
+#Perform KMO Test
+sink(file = "C:/Users/heraa/Documents/University/Doctoral Directed Studies/Guest Lecture/Factor Analysis/KMOtest.txt")
+KMO(efadataset)
+sink(file = NULL)
+
+#Perform Bartlett's test
+sink(file = "C:/Users/heraa/Documents/University/Doctoral Directed Studies/Guest Lecture/Factor Analysis/Bartlettstest.txt")
+bart_spher(efadataset)
+sink(file = NULL)
+
+# Parallel Analysis
+sink(file = "C:/Users/heraa/Documents/University/Doctoral Directed Studies/Guest Lecture/Factor Analysis/ParallelAnalysis.txt")
+paran(efadataset[, -c(1, 2, 3, 4, 5)], cfa=TRUE, graph=TRUE, 
+      color=TRUE, col=c("black","red","blue"))
+sink(file = NULL)
+
+# Scree Plot
+nofactors = fa.parallel(efadataset[, -c(1, 2, 3, 4, 5)], fm="pa", fa="fa")
+nofactors
+
+# Kaiser Criterion
+sink(file = "C:/Users/heraa/Documents/University/Doctoral Directed Studies/Guest Lecture/Factor Analysis/Kaiser.txt")
+nofactors = fa.parallel(efadataset[, -c(1, 2, 3, 4, 5)], fm="pa", fa="fa")
+nofactors
+sink(file = NULL)
+
+#=IF(ABS(B2)<0.3,"",B2)
+
+# 11 Factors
+sink(file = "C:/Users/heraa/Documents/University/Doctoral Directed Studies/Guest Lecture/Factor Analysis/Results/11Factors.txt")
+fa(efadataset, nfactors = 11, rotate =  "oblimin")
+sink(file = NULL)
+
+# 6 Factors
+sink(file = "C:/Users/heraa/Documents/University/Doctoral Directed Studies/Guest Lecture/Factor Analysis/Results/6Factors.txt")
+fa(efadataset, nfactors = 6, rotate =  "oblimin")
+sink(file = NULL)
+
+# 5 Factors
+sink(file = "C:/Users/heraa/Documents/University/Doctoral Directed Studies/Guest Lecture/Factor Analysis/Results/5Factors.txt")
+fa(efadataset, nfactors = 5, rotate =  "oblimin")
+sink(file = NULL)
+
+#Remove Publications (<h2)
+sink(file = "C:/Users/heraa/Documents/University/Doctoral Directed Studies/Guest Lecture/Factor Analysis/Results/5Factors-1.txt")
+fa(efadataset[, -c(4)], nfactors = 5, rotate =  "oblimin")
+sink(file = NULL)
+
